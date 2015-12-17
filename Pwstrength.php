119f13278700e0310c4064b83f3d2543a3a39cd0
murav <muravshchyk@gmail.com>
2015-12-17 15:51:45 +0200

    make good pwstrength

--- Pwstrength.php
+++ Pwstrength.php
@@ -7,5 +7,30 @@ use yii\widgets\InputWidget;
 
 
 class Pwstrength extends InputWidget
-{ 
+{
+    public function init()
+    {
+        parent::init();
+        $this->registerAssets();
+    }
+
+    /**
+     * @return string
+     */
+    public function run()
+    {
+            Html::addCssClass($this->options, 'form-control');
+            return Html::activeTextInput($this->model, $this->attribute, $this->options);
+    }
+
+    /**
+     * Registers the needed assets
+     */
+    public function registerAssets()
+    {
+
+        PwstrengthAsset::register($this->view);
+        Yii::$app->view->registerJs("jQuery('#" . $this->options['id'] . "').pwstrength({common: {minChar: 8, usernameField: '#signupform-nickname'}});");
+    }
+
 }
