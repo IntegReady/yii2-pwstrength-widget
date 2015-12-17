88a66e3f36d7a205e2ac8643b0397d488a4120ce
murav <muravshchyk@gmail.com>
2015-12-17 15:08:27 +0200

    some edit

--- Pwstrength.php
+++ Pwstrength.php
@@ -5,12 +5,7 @@ use Yii;
 use yii\helpers\Html;
 use yii\widgets\InputWidget;
 
-/**
- * Created by PhpStorm.
- * User: Asus2
- * Date: 07-Dec-15
- * Time: 5:37 PM
- */
+
 class Pwstrength extends InputWidget
 { 
 }
